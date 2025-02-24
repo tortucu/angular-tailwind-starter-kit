be56744f4a6a27335a42e599a96081270a011168
tortucu <tortuc@outlook.com>
2025-02-24 23:44:48 +0200

    add tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-tailwind-starter-kit';
+  title = 'Angular tailwind starter kit';
 }
